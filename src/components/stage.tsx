2a22dbc8356e2225859169acc95c670c803ca35c
Justin Fowler <jusfowle@gmail.com>
2022-10-03 22:15:33 -0400

    Update dependencies and build scripts

--- src/components/stage.tsx
+++ src/components/stage.tsx
@@ -3,7 +3,7 @@ import { IStage } from './types';
 
 export const Stage = ({ stage }: { stage: IStage }) => {
 	return (
-		<main className='grid grid-cols-12 gap-0 bg-slate-900 auto-cols w-3/4 xs:w-[60%] sm:w-7/12 md:w-1/2 lg:w-[40%] xl:w-1/4 border border-slate-400'>
+		<main className='grid grid-cols-12 gap-0 bg-slate-900 w-3/4 xs:w-[60%] sm:w-7/12 md:w-1/2 lg:w-[40%] xl:w-1/4 border border-slate-400'>
 			{stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} status={cell[1]} />))}
 		</main>
 	);
